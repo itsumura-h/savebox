63310250993ce3f6b0073252dbfa1d6c7f1f2d24
Takuya Matsuda <takuya108817@gmail.com>
2021-08-29 12:06:35 +0900

    useEffectのエラー消す

--- src/client/src/Pages/ProjectIndex.js
+++ src/client/src/Pages/ProjectIndex.js
@@ -19,42 +19,49 @@ const PorjectIndex=()=>{
     setStorageValue(response)
   };
 
-  useEffect(async() => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // const deployedNetwork = Savebox.networks[networkId];
-      // const instance = new web3.eth.Contract(
-      //   Savebox.abi,
-      //   deployedNetwork && deployedNetwork.address,
-      // );
-      // console.log(deployedNetwork.address) Cannot read property 'address' of undefined
-      // instance.options.address = "0x2370f9d504c7a6E775bf6E14B3F12846b594cD53"
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      // this.setState({ web3, accounts, contract: instance });
-      setWeb3(instance);
-      setAccounts(instance);
-      setContract(instance);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+  useEffect(() => {
+    async function connectMetamask() {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
+        console.log('web3: %d', web3)
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+        console.log('accounts: %d', accounts)
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId();
+  
+        const deployedNetwork = SimpleStorageContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          SimpleStorageContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+        console.log('instance: %d', instance)
+  
+  
+        // const deployedNetwork = Savebox.networks[networkId];
+        // const instance = new web3.eth.Contract(
+        //   Savebox.abi,
+        //   deployedNetwork && deployedNetwork.address,
+        // );
+        // console.log(deployedNetwork.address) Cannot read property 'address' of undefined
+        // instance.options.address = "0x2370f9d504c7a6E775bf6E14B3F12846b594cD53"
+  
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        // this.setState({ web3, accounts, contract: instance });
+        setWeb3(web3);
+        setAccounts(accounts);
+        setContract(instance);
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
     }
+    connectMetamask()
   }, [])
 
   return (
